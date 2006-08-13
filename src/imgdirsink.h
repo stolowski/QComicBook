0e7e8f93955bfd12214762270ea8ad0640bd9e9e
yogin <devnull@localhost>
2006-08-13 13:01:03 +0000

    Aktualizacja dat copyright.

--- src/imgdirsink.h
+++ src/imgdirsink.h
@@ -1,7 +1,7 @@
 /*
  * This file is a part of QComicBook.
  *
- * Copyright (C) 2005 Pawel Stolowski < yogin@linux.bydg.org>
+ * Copyright (C) 2005-2006 Pawel Stolowski < yogin@linux.bydg.org>
  *
  * QComicBook is free software; you can redestribute it and/or modify it
  * under terms of GNU General Public License by Free Software Foundation.
--- src/imlibimage.cpp
+++ src/imlibimage.cpp
@@ -1,3 +1,15 @@
+/*
+ * This file is a part of QComicBook.
+ *
+ * Copyright (C) 2005-2006 Pawel Stolowski <yogin@linux.bydg.org>
+ *
+ * QComicBook is free software; you can redestribute it and/or modify it
+ * under terms of GNU General Public License by Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See GPL for more details.
+ */
+
 #include "imlibimage.h"
 #include <iostream>
 #include <qpaintdevice.h>
