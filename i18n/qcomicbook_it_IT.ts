2fab9f511a20c1f8258801fc00e2a9a0c96d3a86
Pawel Stolowski <stolowski@gmail.com>
2010-05-22 19:27:07 +0200

    Added italian translation.

---
+++ i18n/qcomicbook_it_IT.ts
@@ -0,0 +1,195 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>BookmarkManager</name>
+    <message>
+        <source>Deleting bookmarks</source>
+        <translation>Eliminando segnalibri</translation>
+    </message>
+    <message>
+        <source>Delete selected bookmarks?</source>
+        <translation>Elimina i segnalibri selezionati?</translation>
+    </message>
+</context>
+<context>
+    <name>ComicBookInfo</name>
+    <message>
+        <source>QComicBook info</source>
+        <translation>Informazioni su QComicBook</translation>
+    </message>
+</context>
+<context>
+    <name>ComicMainWindow</name>
+    <message>
+        <source>Escape</source>
+        <translation>Escape</translation>
+    </message>
+    <message>
+        <source>Alt+t</source>
+        <translation>Alt+t</translation>
+    </message>
+    <message>
+        <source>Recently opened</source>
+        <translation>Aperti recentemente</translation>
+    </message>
+    <message>
+        <source>Leave QComicBook?</source>
+        <translation>Esci da QComicBook?</translation>
+    </message>
+    <message>
+        <source>Do you really want to quit QComicBook?</source>
+        <translation>Vuoi veramente uscire da QComicBook?</translation>
+    </message>
+    <message>
+        <source>no images found</source>
+        <translation>nessuna immagine trovata</translation>
+    </message>
+    <message>
+        <source>unknown archive</source>
+        <translation>archivio sconosciuto</translation>
+    </message>
+    <message>
+        <source>can&apos;t access directory</source>
+        <translation>impossibile accedere alla cartella</translation>
+    </message>
+    <message>
+        <source>file/directory not found</source>
+        <translation>file/cartella non trovata</translation>
+    </message>
+    <message>
+        <source>archive not supported</source>
+        <translation>archivio non supportato</translation>
+    </message>
+    <message>
+        <source>archive extractor exited with error</source>
+        <translation>l'estrattore di archivi è terminato con errori</translation>
+    </message>
+    <message>
+        <source>QComicBook error</source>
+        <translation>errore QComicBook</translation>
+    </message>
+    <message>
+        <source>Error opening comicbook</source>
+        <translation>Errore nell'apertura del fumetto</translation>
+    </message>
+    <message>
+        <source>Choose a directory</source>
+        <translation>Scegli cartella</translation>
+    </message>
+    <message>
+        <source>Choose a file</source>
+        <translation>Scegli file</translation>
+    </message>
+    <message>
+        <source>Page</source>
+        <translation>Pagina</translation>
+    </message>
+    <message>
+        <source>Removing bookmark</source>
+        <translation>Rimuovendo segnalibro</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove bookmark
+for this comic book?</source>
+        <translation>Vuoi veramente rimuovere il segnalibro per questo fumetto?</translation>
+    </message>
+    <message>
+        <source>Save image as</source>
+        <translation>Salva l'immagine come</translation>
+    </message>
+    <message>
+        <source>page</source>
+        <translation>pagina</translation>
+    </message>
+    <message>
+        <source>Error saving image</source>
+        <translation>Errore durante il salvataggio dell'immagine</translation>
+    </message>
+    <message>
+        <source>Comic book not found</source>
+        <translation>Fumetto non trovato</translation>
+    </message>
+    <message>
+        <source>Selected bookmark points to
+non-existing comic book
+Do you want to remove it?</source>
+        <translation>Il segnalibro selezionato punta
+ad un fumetto inesistente
+Vuoi rimuoverlo?</translation>
+    </message>
+    <message>
+        <source>Can&apos;t initialize QComicBook directories</source>
+        <translation type="unfinished">Inizializzazione cartelle QComicBook fallita</translation>
+    </message>
+</context>
+<context>
+    <name>ImgDirSink</name>
+    <message>
+        <source>bytes</source>
+        <translation>byte</translation>
+    </message>
+    <message>
+        <source>Kbytes</source>
+        <translation>kByte</translation>
+    </message>
+    <message>
+        <source>Mbytes</source>
+        <translation>MByte</translation>
+    </message>
+</context>
+<context>
+    <name>StatusBar</name>
+    <message>
+        <source>Page</source>
+        <translation>Pagina</translation>
+    </message>
+</context>
+<context>
+    <name>SystemInfoDialog</name>
+    <message>
+        <source>Supported archivers</source>
+        <translation>Archivi supportati</translation>
+    </message>
+    <message>
+        <source>Archive type</source>
+        <translation>Tipo di archivio</translation>
+    </message>
+    <message>
+        <source>Supported file extensions</source>
+        <translation>Estensioni supportate</translation>
+    </message>
+    <message>
+        <source>Unsupported archivers</source>
+        <translation>Archivi non supportati</translation>
+    </message>
+    <message>
+        <source>Required executable(s)</source>
+        <translation>Eseguibili richiesti</translation>
+    </message>
+    <message>
+        <source>Hint</source>
+        <translation>Suggerimento</translation>
+    </message>
+    <message>
+        <source>Some archives are not supported because essential external utilities are missing. Please install required executables and restart QComicBook. Note: if more than one executable is listed for given archive type, then it is sufficient to install just one of them.</source>
+        <translation>Alcuni archivi non sono supportati a causa dell'assenza di applicazioni esterne. Installa gli eseguibili richiesti e riavvia QComicBook. Nota: se più di un eseguibile è elencato per un tipo di archivio, è sufficiente installare uno di essi.</translation>
+    </message>
+    <message>
+        <source>Supported image formats</source>
+        <translation>Formati supportati</translation>
+    </message>
+</context>
+<context>
+    <name>ThumbnailsView</name>
+    <message>
+        <source>Go to</source>
+        <translation>Vai a</translation>
+    </message>
+</context>
+<context>
+    <name>ThumbnailsWindow</name>
+    <message>
+        <source>Thumbnails</source>
+        <translation>Miniature</translation>
+    </message>
+</context>
+</TS>
--- i18n/update_ts.sh
+++ i18n/update_ts.sh
@@ -2,7 +2,7 @@
 #
 # This script updates/creates translation (.ts) files.
 # To create new translation, execute:
-#   lupdate qcomicbook_xx_XX.ts
+#   ./update_ts.sh qcomicbook_xx_XX.ts
 # (replace xx_XX with your country code).
 #
 # Must be executed in lang subdirectory.
