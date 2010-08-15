f8c1444722841093a5e52103ebe82a1b209bb4d0
Pawel Stolowski <stolowski@gmail.com>
2010-08-15 17:50:56 +0200

    Initial implementation of frame sorting, based on X & Y coordinates only.

--- src/FrameDetect/ComicFrameList.cpp
+++ src/FrameDetect/ComicFrameList.cpp
@@ -1,4 +1,17 @@
+/*
+ * This file is a part of QComicBook.
+ *
+ * Copyright (C) 2005-2010 Pawel Stolowski <stolowski@gmail.com>
+ *
+ * QComicBook is free software; you can redestribute it and/or modify it
+ * under terms of GNU General Public License by Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See GPL for more details.
+ */
+
 #include <ComicFrameList.h>
+#include <algorithm>
 #include <QDebug>
 
 using namespace QComicBook;
@@ -14,6 +27,23 @@ ComicFrameList::~ComicFrameList()
 
 }
 
+struct CompareFrames
+{
+	bool operator()(const ComicFrame &f1, const ComicFrame &f2)
+	{
+		const int m = 10;
+
+		if (std::abs(f1.yPos() - f2.yPos()) < m)
+		{
+			return f1.xPos() < f2.xPos();
+		}
+		else
+		{
+			return f1.yPos() < f2.yPos();
+		}
+	}
+};
+
 void ComicFrameList::append(const ComicFrame &f)
 {
 	m_frames.append(f);
@@ -22,6 +52,7 @@ void ComicFrameList::append(const ComicFrame &f)
 void ComicFrameList::sort()
 {
 	qDebug() << "frames sort";
+	std::sort(m_frames.begin(), m_frames.end(), CompareFrames());
 }
 
 const ComicFrame& ComicFrameList::operator[](int idx)
