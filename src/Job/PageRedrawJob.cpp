72db6b1f5ead2548d24d7390edd49e546ece913f
Pawel Stolowski <stolowski@gmail.com>
2011-10-15 13:46:52 +0100

    Added licencse headers to new job relates files.

--- src/Job/PageRedrawJob.cpp
+++ src/Job/PageRedrawJob.cpp
@@ -1,3 +1,15 @@
+/*
+ * This file is a part of QComicBook.
+ *
+ * Copyright (C) 2005-2011 Pawel Stolowski <stolowski@gmail.com>
+ *
+ * QComicBook is free software; you can redestribute it and/or modify it
+ * under terms of GNU General Public License by Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY. See GPL for more details.
+ */
+
 #include <QImage>
 #include <QPainter>
 #include "../ComicBookDebug.h"
